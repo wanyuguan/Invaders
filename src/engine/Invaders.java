bbfa14730f81ff209f0970e956470d5c3a8b7251
JueVioleGrace <Shadowlinker@gmail.com>
2015-08-03 12:53:12 -0700

    The first issue#3 has some problems. But now is good to go.

--- src/engine/Invaders.java
+++ src/engine/Invaders.java
@@ -19,25 +19,33 @@ public class Invaders {
 
         // Hold on to all of the game's information
         GameState gameState = new GameState(1, 0, Constants.MAX_LIVES, 0, 0);
-        
+
         // Show Title Screen below this line
         Screen screen = new TitleScreen();
         screen.show();
-       ScreenType nextScreenType = screen.getNextScreenType();
+        ScreenType nextScreenType = screen.getNextScreenType();
 
-        while( nextScreenType == ScreenType.EndGame) {
-       if (nextScreenType == ScreenType.TitleScreen) {
-            screen = new TitleScreen();
+        while (nextScreenType != ScreenType.EndGame) {
+            if (nextScreenType == ScreenType.TitleScreen) {
+                screen = new TitleScreen();
+                screen.show();
+                nextScreenType = screen.getNextScreenType();
+
+            } else if (nextScreenType == ScreenType.GameScreen) {
+                screen = new GameScreen(gameState, levelSettings);
+                screen.show();
+                nextScreenType = screen.getNextScreenType();
+
+            } else if (nextScreenType == ScreenType.ScoreScreen) {
+                screen = new ScoreScreen(gameState);
+                screen.show();
+                nextScreenType = screen.getNextScreenType();
+
+            } else if (nextScreenType == ScreenType.HighScoreScreen) {
+                screen = new HighScoreScreen();
+                screen.show();
+                nextScreenType = screen.getNextScreenType();
+            }
         }
-        else if( nextScreenType == ScreenType.GameScreen) {
-            screen = new GameScreen(gameState,levelSettings);
-        }
-        else if (nextScreenType == ScreenType.ScoreScreen) {
-               screen = new ScoreScreen(gameState);
-           }
-       else if (nextScreenType == ScreenType.HighScoreScreen) {
-               screen = new HighScoreScreen();
-           }
-       }
     }
 }
